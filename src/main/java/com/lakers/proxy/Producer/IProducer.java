c9d6339260c538bf7cd08ef1675f1de4312c446d
Ma3querader <593661332@qq.com>
2020-05-15 17:30:30 +0800

    基于接口的动态代理(jdk)

---
+++ src/main/java/com/lakers/proxy/Producer/IProducer.java
@@ -0,0 +1,13 @@
+package com.lakers.proxy.Producer;
+
+/**
+ * @Author: panyusheng
+ * @Date: 2020/5/15 17:00
+ * 生产者接口
+ * @Version 1.0
+ */
+public interface IProducer {
+
+    void saleProduct(float money);
+
+}
