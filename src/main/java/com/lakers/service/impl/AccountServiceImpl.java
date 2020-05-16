098e49b40a1a94d367dc7c14d045e7aa1dbfbc20
Ma3querader <593661332@qq.com>
2020-05-16 18:41:08 +0800

    基于xml实现spring aop

--- src/main/java/com/lakers/service/impl/AccountServiceImpl.java
+++ src/main/java/com/lakers/service/impl/AccountServiceImpl.java
@@ -18,62 +18,88 @@ import java.util.List;
  * @Version 1.0
  */
 public class AccountServiceImpl implements AccountService {
+    public List<Account> findAllAccount() {
+        return null;
+    }
+
+    public Account findAccountById(Integer accountId) {
+        return null;
+    }
+
+    public void saveAccount(Account account) {
+        System.out.println("执行了保存");
+    }
+
+    public void updateAccount(Account account) {
+        System.out.println("执行了更新"+account);
+    }
+
+    public void deleteAccount(Integer accountId) {
+        System.out.println("执行了删除");
+    }
+
+    public void transfer(String sourceName, String targetName, Float money) {
+
+    }
 
 //    @Autowired
 //    @Qualifier("accountDaoImpl")
 //    @Resource(name = "accountDaoImpl")
-    private AccountDao accountDao;
+//    private AccountDao accountDao;
+//
+//    private TransactionManager txManager;
+//
+//    public void setAccountDao(AccountDao accountDao) {
+//        this.accountDao = accountDao;
+//    }
+//
+//    public void setTxManager(TransactionManager txManager) {
+//        this.txManager = txManager;
+//    }
+//
+//    public List<Account> findAllAccount() {
+//        try {
+//            txManager.beginTransaction();
+//            List<Account> accounts = accountDao.findAllAccount();
+//            txManager.commit();
+//            return accounts;
+//        } catch (Exception e) {
+//            txManager.rollback();
+//            throw new RuntimeException(e);
+//        } finally {
+//            txManager.release();
+//        }
+//    }
+//
+//    public Account findAccountById(Integer accountId) {
+//        return accountDao.findAccountById(accountId);
+//    }
+//
+//    public void saveAccount(Account account) {
+//        accountDao.saveAccount(account);
+//    }
+//
+//    public void updateAccount(Account account) {
+//        accountDao.updateAccount(account);
+//    }
+//
+//    public void deleteAccount(Integer accountId) {
+//        accountDao.deleteAccount(accountId);
+//    }
+//
+//    public void transfer(String sourceName, String targetName, Float money) {
+//        Account source = accountDao.findByName(sourceName);
+//        Account target = accountDao.findByName(targetName);
+//        source.setMoney(source.getMoney()-money);
+//        target.setMoney(target.getMoney()+money);
+//        //更新两个账户
+//        accountDao.updateAccount(source);
+//        //模拟转账异常
+//        int i=1/0;
+//        accountDao.updateAccount(target);
+//    }
 
-    private TransactionManager txManager;
 
-    public void setAccountDao(AccountDao accountDao) {
-        this.accountDao = accountDao;
-    }
 
-    public void setTxManager(TransactionManager txManager) {
-        this.txManager = txManager;
-    }
-
-    public List<Account> findAllAccount() {
-        try {
-            txManager.beginTransaction();
-            List<Account> accounts = accountDao.findAllAccount();
-            txManager.commit();
-            return accounts;
-        } catch (Exception e) {
-            txManager.rollback();
-            throw new RuntimeException(e);
-        } finally {
-            txManager.release();
-        }
-    }
-
-    public Account findAccountById(Integer accountId) {
-        return accountDao.findAccountById(accountId);
-    }
-
-    public void saveAccount(Account account) {
-        accountDao.saveAccount(account);
-    }
-
-    public void updateAccount(Account account) {
-        accountDao.updateAccount(account);
-    }
-
-    public void deleteAccount(Integer accountId) {
-        accountDao.deleteAccount(accountId);
-    }
-
-    public void transfer(String sourceName, String targetName, Float money) {
-        Account source = accountDao.findByName(sourceName);
-        Account target = accountDao.findByName(targetName);
-        source.setMoney(source.getMoney()-money);
-        target.setMoney(target.getMoney()+money);
-        //更新两个账户
-        accountDao.updateAccount(source);
-        //模拟转账异常
-        int i=1/0;
-        accountDao.updateAccount(target);
-    }
 
 }
---
+++ src/test/java/TestAop.java
@@ -0,0 +1,37 @@
+import com.lakers.domain.Account;
+import com.lakers.service.AccountService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+/**
+ * @Author: panyusheng
+ * @Date: 2020/5/16 17:44
+ * @Version 1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:bean.xml")
+public class TestAop {
+
+    @Autowired
+    private AccountService service;
+
+    @Test
+    public void testFindAll() {
+        List<Account> allAccount = service.findAllAccount();
+        System.out.println(allAccount);
+    }
+
+    @Test
+    public void testSave() {
+        Account account = new Account();
+        account.setName("基尔加丹");
+        service.saveAccount(account);
+    }
+
+}
